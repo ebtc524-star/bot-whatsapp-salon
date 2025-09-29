ae4adc5b7a6d4e360b67dd7c3e70687d0f895da5
ebtc524-star <ebtc524@gmail.com>
2025-09-29 03:16:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Cargar configuración
-const config = require('./src/config.json');
+const config = require('./config.json');
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static('.'));
@@ -317,7 +317,7 @@ app.get('/api/status', (req, res) => {
 
 // Servir panel
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/panel.html'));
+   res.sendFile(path.join(__dirname, 'panel.html'));
 });
 
 // Iniciar servidor
@@ -327,4 +327,5 @@ app.listen(PORT, () => {
     console.log(`📱 Webhook: /webhook`);
     console.log(`🌐 Panel: http://localhost:${PORT}`);
 });
+
 
